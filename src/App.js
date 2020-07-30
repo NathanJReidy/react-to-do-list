aecf895cb843ab39abbb8e9472ef07d502fe625a
Nathan Reidy <nathanjreidy@gmail.com>
2020-07-30 11:33:16 +1000

    Created ToDoItem component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
+import ToDoItem from './ToDoItem';
 
 function App() {
     return (
-        <div>
-            <input type="checkbox" />
-            <p>This is paragraph 1. </p>
-
-            <input type="checkbox" />
-            <p>This is paragraph 2. </p>
-
-            <input type="checkbox" />
-            <p>This is paragraph 2. </p>
-
-            <input type="checkbox" />
-            <p>This is paragraph 2. </p>
-
-        </div>
+        <ToDoItem />
     )
 }
 
---
+++ src/ToDoItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function ToDoItem() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>This is paragraph 1. </p>
+
+            <input type="checkbox" />
+            <p>This is paragraph 2. </p>
+
+            <input type="checkbox" />
+            <p>This is paragraph 2. </p>
+
+            <input type="checkbox" />
+            <p>This is paragraph 2. </p>
+        </div>
+    )
+}
+
+export default ToDoItem; 
