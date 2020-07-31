9c410310d36e8609b4032204b5476404154f89e6
Nathan Reidy <nathanjreidy@gmail.com>
2020-07-31 20:01:50 +1000

    Changed functional App.js to class instead

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import ToDoItem from './ToDoItem';
 import todosData from './ToDosData';
+import { render } from '@testing-library/react';
 
-function App() {
-    const toDos = todosData.map(item => <ToDoItem 
-        item={item}
-        key={item.id} 
-        />)
+// function App() {
+//     const toDos = todosData.map(item => <ToDoItem 
+//         item={item}
+//         key={item.id} 
+//         />)
     
-    return (
-        <div className="todo-list">
-            {toDos}
-        </div>
-    )
+//     return (
+//         <div className="todo-list">
+//             {toDos}
+//         </div>
+//     )
+// } 
+
+
+class App extends React.Component {
+    constructor() {
+        super()
+
+        this.state = todosData.map(item => <ToDoItem 
+            item={item}
+            key={item.id} 
+        />)
+
+        }
+        render() {
+            return (
+                <div className="todo-list">
+                    {this.state}
+                </div>
+            )
+        }
 }
+
 
 
 export default App;
