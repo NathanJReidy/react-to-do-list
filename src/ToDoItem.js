b3fb3389f9c13fcbcbaede418593e97e68e0698a
Nathan Reidy <nathanjreidy@gmail.com>
2020-07-31 21:03:13 +1000

    added event handler to checkbox in ToDoItem.js

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -3,7 +3,11 @@ import React from 'react';
 function ToDoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed ? "checked" : ""}/>
+            <input 
+            type="checkbox" 
+            onChange={() => console.log("Changed!")} 
+            checked={props.item.completed ? "checked" : ""}
+            />
             <p>{props.item.text} </p>
         </div>
     )
